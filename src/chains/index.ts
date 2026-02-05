12f8c91025269163929b0754cd137eb8b22b27af
Shaweeen <79692938+Shaweeen@users.noreply.github.com>
2026-02-05 14:43:08 +0800

    feat: Add chains index

---
+++ src/chains/index.ts
@@ -0,0 +1,4 @@
+export { BaseChainMonitor } from './base';
+export { EVMChainMonitor, createEVMMonitor, evmEventEmitter } from './ethereum';
+export { SolanaMonitor, solanaEventEmitter } from './solana';
+export { BitcoinMonitor, bitcoinEventEmitter } from './bitcoin';
