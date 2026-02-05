3a9052b30232ef53bc2e79b510e5fb7958a465db
Shaweeen <79692938+Shaweeen@users.noreply.github.com>
2026-02-05 07:49:47 +0000

    fix: export IChainMonitor from src/chains/index.ts

--- src/chains/index.ts
+++ src/chains/index.ts
@@ -2,3 +2,4 @@ export { BaseChainMonitor } from './base';
 export { EVMChainMonitor, createEVMMonitor, evmEventEmitter } from './ethereum';
 export { SolanaMonitor, solanaEventEmitter } from './solana';
 export { BitcoinMonitor, bitcoinEventEmitter } from './bitcoin';
+export { IChainMonitor } from '../types';
